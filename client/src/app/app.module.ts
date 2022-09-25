1ef26c601fcc09b383a75814a118108353ebdf9e
Pratyush <prtshkmr866@gmail.com>
2022-09-25 15:32:29 +0530

    Added Nav Componet and Forms Module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule } from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
