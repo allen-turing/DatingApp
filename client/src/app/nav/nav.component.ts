3aac103b1c49612d8ffbafcbe132a463c114e2e4
Pratyush <prtshkmr866@gmail.com>
2022-11-14 01:09:54 +0530

    Making Login Persistant on page refresh also
    In account.service we retrive the user and sets it in localstorage
    In App componet we are making effort to get token from local storage
    and then we are setting that in our account service, our nav component
    subscribe to that observable and then sets loggedIn status to current
    user

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,26 +4,42 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  model: any = {}
-  loggedIn : boolean;
-  constructor(private accountServices: AccountService) { }
+  model: any = {};
+  loggedIn: boolean;
+  constructor(private accountServices: AccountService) {}
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
-    this.accountServices.login(this.model).subscribe(response => {
-      console.log(response);
-      this.loggedIn = true;
-    },error => {
-      console.log(error);
-    })
+    this.accountServices.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   logout() {
+    this.accountServices.logout();
     this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountServices.currentUser$.subscribe(
+      (user) => {
+        this.loggedIn = !!user;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 }
